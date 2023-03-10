41043c1c8381a16fd2a3fa0bf345a54c0475afef
omgshalihin <shalihinsgp@gmail.com>
2023-03-10 21:39:24 +0100

    substring between [] and made lowercase

--- src/main/java/org/example/logLevels/LogLevels.java
+++ src/main/java/org/example/logLevels/LogLevels.java
@@ -5,12 +5,13 @@ import java.util.Arrays;
 public class LogLevels {
 
     public static String message(String logLine) {
-        String[] a = logLine.split(":");
-        return a[1].trim();
+        String[] logArray = logLine.split(":");
+        return logArray[1].trim();
     }
 
     public static String logLevel(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.logLevel() method");
+        String[] logArray = logLine.split(":");
+        return logArray[0].substring(1, logArray[0].length()-1).toLowerCase();
     }
 
     public static String reformat(String logLine) {
--- src/test/java/org/example/logLevels/LogLevelsTest.java
+++ src/test/java/org/example/logLevels/LogLevelsTest.java
@@ -13,5 +13,33 @@ class LogLevelsTest {
         assertEquals(LogLevels.message(message1), "Invalid operation");
         assertEquals(LogLevels.message(message2), "Disk almost full");
     }
+    @Test
+    public void reformat_with_leading_and_trailing_white_space() {
+        assertEquals(LogLevels.reformat("[ERROR]: \t Corrupt disk\t \t \r\n"), "Corrupt disk (error)");
+    }
+    @Test
+    public void info_log_level() {
+        assertEquals(LogLevels.logLevel("[INFO]: Timezone changed"), "info");
+    }
+    @Test
+    public void error_reformat() {
+        assertEquals(LogLevels.reformat("[ERROR]: Segmentation fault"), "Segmentation fault (error)");
+    }
+    @Test
+    public void error_log_level() {
+        assertEquals(LogLevels.logLevel("[ERROR]: Disk full"), "error");
+    }
+    @Test
+    public void info_reformat() {
+        assertEquals(LogLevels.reformat("[INFO]: Disk defragmented"), "Disk defragmented (info)");
+    }
+    @Test
+    public void warning_reformat() {
+        assertEquals(LogLevels.reformat("[WARNING]: Decreased performance"), "Decreased performance (warning)");
+    }
+    @Test
+    public void warning_log_level() {
+        assertEquals(LogLevels.logLevel("[WARNING]: Unsafe password"), "warning");
+    }
 
 }
