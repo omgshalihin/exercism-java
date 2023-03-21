bf2aba508f94e78e6bc1b960c6de997eabf33cff
omgshalihin <shalihinsgp@gmail.com>
2023-03-21 16:05:44 +0100

    implemented tests

--- src/main/java/org/example/resistorColor/ResistorColor.java
+++ src/main/java/org/example/resistorColor/ResistorColor.java
@@ -1,11 +1,11 @@
 package org.example.resistorColor;
 
 public class ResistorColor {
-    int colorCode(String color) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    static int colorCode(String color) {
+        return 0;
     }
 
-    String[] colors() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    static String[] colors() {
+        return null;
     }
 }
--- src/test/java/org/example/resistorColor/ResistorColorTest.java
+++ src/test/java/org/example/resistorColor/ResistorColorTest.java
@@ -1,9 +1,35 @@
 package org.example.resistorColor;
 
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ResistorColorTest {
 
-
-
+    private ResistorColor resistorColor;
+    @Before
+    public void setup() {
+        resistorColor = new ResistorColor();
+    }
+    @Test
+    public void testBlackColorCode() {
+        assertEquals(resistorColor.colorCode("black"), 0);
+    }
+    @Ignore("Remove to run test")
+    @Test
+    public void testWhiteColorCode() {
+        assertEquals(resistorColor.colorCode("white"), 9);
+    }
+    @Ignore("Remove to run test")
+    @Test
+    public void testOrangeColorCode() {
+        assertEquals(resistorColor.colorCode("orange"), 3);
+    }
+    @Ignore("Remove to run test")
+    @Test
+    public void testColors() {
+        assertEquals(resistorColor.colors(), new String[]{"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"});
+    }
 }
