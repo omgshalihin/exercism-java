5f2e7e7fcf3c0b9a5eab376d8950df21dcfa167e
omgshalihin <shalihinsgp@gmail.com>
2023-03-28 10:26:11 +0200

    added method and enum

---
+++ src/main/java/org/example/perfectNumbers/Classification.java
@@ -0,0 +1,5 @@
+package org.example.perfectNumbers;
+
+public enum Classification {
+    DEFICIENT, PERFECT, ABUNDANT
+}
---
+++ src/main/java/org/example/perfectNumbers/NaturalNumber.java
@@ -0,0 +1,9 @@
+package org.example.perfectNumbers;
+
+public class NaturalNumber {
+
+    public Classification getClassification() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+}
+
