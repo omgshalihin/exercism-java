f167fe837d3f08e4918936e463477ec8c0b8ce18
omgshalihin <shalihinsgp@gmail.com>
2023-04-02 11:55:32 +0200

    added a method

---
+++ src/main/java/org/example/secretHandshake/HandshakeCalculator.java
@@ -0,0 +1,10 @@
+package org.example.secretHandshake;
+
+import sun.misc.Signal;
+
+public class HandshakeCalculator {
+
+    List<Signal> calculateHandshake(int number) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+}
---
+++ src/test/java/org/example/secretHandshake/HandshakeCalculatorTest.java
@@ -0,0 +1,7 @@
+package org.example.secretHandshake;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HandshakeCalculatorTest {
+
+}
