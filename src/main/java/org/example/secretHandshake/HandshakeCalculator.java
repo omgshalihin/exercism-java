ca6d60cd0b85ff7870a5752c9578036580e779c5
omgshalihin <shalihinsgp@gmail.com>
2023-04-02 13:36:17 +0200

    ALL TESTS PASSED

--- src/main/java/org/example/secretHandshake/HandshakeCalculator.java
+++ src/main/java/org/example/secretHandshake/HandshakeCalculator.java
@@ -1,10 +1,47 @@
 package org.example.secretHandshake;
 
-import sun.misc.Signal;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class HandshakeCalculator {
 
+    static int decimalToBinary(int number) {
+        return Integer.parseInt(Integer.toBinaryString(number));
+    }
+
+    public List<Signal> getSignal(int number) {
+
+        List<Signal> signalList = new ArrayList<>();
+
+        StringBuilder sb = new StringBuilder(String.valueOf(decimalToBinary(number)));
+        String binaryStringReversed = sb.reverse().toString();
+
+        if (binaryStringReversed.charAt(0) == '1') {
+            signalList.add(Signal.WINK);
+        }
+
+        if (binaryStringReversed.length() >= 2 && binaryStringReversed.charAt(1) == '1') {
+            signalList.add(Signal.DOUBLE_BLINK);
+        }
+
+        if (binaryStringReversed.length() >= 3 && binaryStringReversed.charAt(2) == '1') {
+            signalList.add(Signal.CLOSE_YOUR_EYES);
+        }
+
+        if (binaryStringReversed.length() >= 4 && binaryStringReversed.charAt(3) == '1') {
+            signalList.add(Signal.JUMP);
+        }
+
+        if (binaryStringReversed.length() >= 5 && binaryStringReversed.charAt(4) == '1') {
+            Collections.reverse(signalList);
+        }
+
+        return signalList;
+    }
+
     List<Signal> calculateHandshake(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return getSignal(number);
     }
 }
--- src/test/java/org/example/secretHandshake/HandshakeCalculatorTest.java
+++ src/test/java/org/example/secretHandshake/HandshakeCalculatorTest.java
@@ -1,7 +1,26 @@
 package org.example.secretHandshake;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class HandshakeCalculatorTest {
+
+    private final HandshakeCalculator handshakeCalculator = new HandshakeCalculator();
+
+    @Test
+    public void testThatInput9YieldsAWinkAndJump() {
+        assertEquals(handshakeCalculator.calculateHandshake(9),new LinkedList<>(List.of(Signal.WINK, Signal.JUMP)));
+    }
+
+    @Test
+    public void testAnInputThatYieldsTwoReversedActions() {
+        assertEquals(handshakeCalculator.calculateHandshake(19), new LinkedList<>(List.of(Signal.DOUBLE_BLINK, Signal.WINK)));
+    }
+
 
 }
