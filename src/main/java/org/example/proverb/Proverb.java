2855c766a3a6a63605b58ae39c1e0d18a2b0ea4d
omgshalihin <shalihinsgp@gmail.com>
2023-04-06 17:41:32 +0200

    added methods

---
+++ src/main/java/org/example/proverb/Proverb.java
@@ -0,0 +1,12 @@
+package org.example.proverb;
+
+public class Proverb {
+
+    Proverb(String[] words) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    String recite() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+}
