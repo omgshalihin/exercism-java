ddbbf9d3a7e270f33242e047964d374f65f5bc30
omgshalihin <shalihinsgp@gmail.com>
2023-04-06 18:11:25 +0200

    all tests passed

--- src/main/java/org/example/proverb/Proverb.java
+++ src/main/java/org/example/proverb/Proverb.java
@@ -1,12 +1,25 @@
 package org.example.proverb;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Proverb {
 
+    private final String[] words;
+
     Proverb(String[] words) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.words = words;
     }
 
     String recite() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return IntStream.range(0, words.length)
+                .mapToObj(this::format)
+                .collect(Collectors.joining("\n"));
+    }
+
+    private String format(int s) {
+        return s != words.length - 1 ?
+                "For want of a " + words[s] + " the " + words[s + 1] + " was lost."
+                : "And all for the want of a " + words[0] + ".";
     }
 }
--- src/test/java/org/example/proverb/ProverbTest.java
+++ src/test/java/org/example/proverb/ProverbTest.java
@@ -1,7 +1,56 @@
 package org.example.proverb;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ProverbTest {
+
+    @Test
+    public void zeroWordsAreGiven() {
+        String[] words  = new String[0];
+        assertEquals(new Proverb(words).recite(), "");
+    }
+    @Test
+    public void singlePieceOfProverb() {
+        String[] words  = new String[]{"nail"};
+        assertEquals(new Proverb(words).recite(), ("And all for the want of a nail."));
+    }
+    @Test
+    public void twoPiecesOfProverb() {
+        String[] words  = new String[]{"nail", "shoe"};
+        assertEquals(new Proverb(words).recite(), (
+                "For want of a nail the shoe was lost.\n" +
+                        "And all for the want of a nail."));
+    }
+    @Test
+    public void shortChainOfConsequences() {
+        String[] words  = new String[]{"nail", "shoe", "horse"};
+        assertEquals(new Proverb(words).recite(), (
+                "For want of a nail the shoe was lost.\n" +
+                        "For want of a shoe the horse was lost.\n" +
+                        "And all for the want of a nail."));
+    }
+    @Test
+    public void fullProverb() {
+        String[] words  = new String[]{"nail", "shoe", "horse", "rider", "message", "battle", "kingdom"};
+        assertEquals(new Proverb(words).recite(), (
+                "For want of a nail the shoe was lost.\n" +
+                        "For want of a shoe the horse was lost.\n" +
+                        "For want of a horse the rider was lost.\n" +
+                        "For want of a rider the message was lost.\n" +
+                        "For want of a message the battle was lost.\n" +
+                        "For want of a battle the kingdom was lost.\n" +
+                        "And all for the want of a nail."));
+    }
+    @Test
+    public void fourPiecesModernizedProverb() {
+        String[] words  = new String[]{"pin", "gun", "soldier", "battle"};
+        assertEquals(new Proverb(words).recite(), (
+                "For want of a pin the gun was lost.\n" +
+                        "For want of a gun the soldier was lost.\n" +
+                        "For want of a soldier the battle was lost.\n" +
+                        "And all for the want of a pin."));
+    }
 
 }
