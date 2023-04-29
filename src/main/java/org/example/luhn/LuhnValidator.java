0ff1e0ffa9f65e441061f3ed90f2f6f89a8fd4a3
omgshalihin <shalihinsgp@gmail.com>
2023-04-29 17:22:46 +0200

    all tests passed

--- src/main/java/org/example/luhn/LuhnValidator.java
+++ src/main/java/org/example/luhn/LuhnValidator.java
@@ -1,12 +1,61 @@
 package org.example.luhn;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LuhnValidator {
 
     boolean isValid(String candidate) {
-        System.out.println(candidate);
-        if (candidate.length() <= 1) {
+
+        String candidateWithoutSpaces = candidate.replaceAll("\\s", "");
+
+        boolean containsNonDigit = candidateWithoutSpaces.replaceAll("[0-9]", "").length() > 0;
+
+        if (candidateWithoutSpaces.length() <= 1) {
             return false;
         }
-        return true;
+        if (containsNonDigit) {
+            return false;
+        }
+
+        StringBuilder sb = new StringBuilder(candidateWithoutSpaces);
+        sb.reverse();
+        String[] candidateArray = sb.toString().split("");
+
+
+        List<String> toDouble = new ArrayList<>();
+        List<String> nonDouble = new ArrayList<>();
+        List<String> resultOfDoubling = new ArrayList<>();
+
+        for (int i = 0; i < candidateWithoutSpaces.length(); i++) {
+            if (i % 2 != 0) {
+                toDouble.add(candidateArray[i]);
+            } else {
+                nonDouble.add(candidateArray[i]);
+            }
+        }
+
+        toDouble.forEach(i -> {
+            int doubleNumber = Integer.parseInt(i) * 2;
+            if (doubleNumber > 9) {
+                resultOfDoubling.add(String.valueOf(doubleNumber - 9));
+            } else {
+                resultOfDoubling.add(String.valueOf(doubleNumber));
+            }
+        });
+
+        int sumResultOfDoubling = 0;
+        int sumNonDouble = 0;
+
+        for (var number : resultOfDoubling) {
+            sumResultOfDoubling += Integer.parseInt(number);
+        }
+        for (var number : nonDouble) {
+            sumNonDouble += Integer.parseInt(number);
+        }
+
+        int sumOfAllDigits = sumResultOfDoubling + sumNonDouble;
+
+        return sumOfAllDigits % 10 == 0;
     }
 }
--- src/test/java/org/example/luhn/LuhnValidatorTest.java
+++ src/test/java/org/example/luhn/LuhnValidatorTest.java
@@ -1,17 +1,14 @@
 package org.example.luhn;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class LuhnValidatorTest {
 
-    private LuhnValidator luhnValidator;
-    @BeforeEach
-    public void setUp() {
-        this.luhnValidator = new LuhnValidator();
-    }
+    LuhnValidator luhnValidator = new LuhnValidator();
+
     @Test
     public void testSingleDigitStringInvalid() {
         assertFalse(luhnValidator.isValid("1"));
@@ -23,6 +20,34 @@ class LuhnValidatorTest {
     @Test
     public void testSimpleValidSINReversedRemainsValid() {
         assertTrue(luhnValidator.isValid("059"));
+    }
+    @Test
+    public void testSimpleValidSINReversedBecomesInvalid() {
+        assertTrue(luhnValidator.isValid("59"));
+    }
+    @Test
+    public void givenValidExample() {
+        assertTrue(luhnValidator.isValid("4539 3195 0343 6467"));
+    }
+    @Test
+    public void givenInvalidExample() {
+        assertFalse(luhnValidator.isValid("8273 1232 7352 0569"));
+    }
+    @Test
+    public void testValidCanadianSINValid() {
+        assertTrue(luhnValidator.isValid("055 444 285"));
+    }
+    @Test
+    public void testStringContainingPunctuationInvalid() {
+        assertFalse(luhnValidator.isValid("055-444-285"));
+    }
+    @Test
+    public void testUsingASCIIValueForNonDoubledNonDigitNotAllowed() {
+        assertFalse(luhnValidator.isValid("055b 444 285"));
+    }
+    @Test
+    public void testStringContainingSymbolsInvalidJavaTrackSpecific() {
+        assertFalse(luhnValidator.isValid("85&"));
     }
 
 }
